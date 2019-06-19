159e501cff01f6376613317953c09f01c0de8542
Justin Leniger <justin.leniger@gmail.com>
2019-06-18 21:07:14 -0600

    promise based camera control

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 
-// Serve all the files in '/dist' directory
+var GPhoto = require('./gphoto.js');
+
+var gphoto = new GPhoto();
+
 app.use(express.static('dist'));
 
+app.get('/connect', async function (req, res) {
+    try {
+        await gphoto.connect();
+    }
+    catch {
+        res.sendStatus(500);
+        return;
+    }
+
+    res.sendStatus(200);
+});
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Listening on :3000...');
 });
